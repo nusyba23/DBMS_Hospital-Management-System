a6aef52688e56666f37dce218276faf6ec8f6831
Farida152005 <farida.elogueil@ucalgary.ca>
2024-12-02 12:37:06 -0700

    display hospital table done

--- Website/displayHospitals.php
+++ Website/displayHospitals.php
@@ -1,26 +1,28 @@
 <?php
-    include("database.php");
+include("database.php");
 
-    $sql = "SELECT * FROM Hospital";
-    $result = mysqli_query($conn, $sql);
-    mysqli_close($conn);
+$sql = "SELECT * FROM Hospital";
+$result = mysqli_query($conn, $sql);
+mysqli_close($conn);
 
-    $needheaders = true;
-    if(mysqli_num_rows($result) > 0){
-        while($row = mysqli_fetch_assoc($result)){
-            if($needheaders){
-                $needheaders = false;
-                foreach($row as $key => $value){
-                    //within quotes need proper html for table hearder $key = attribute
-                    echo "{$key}   ";
-                }
-                echo "<br>";
+$needheaders = true;
+if (mysqli_num_rows($result) > 0) {
+    echo "<table border='1' style='border-collapse: collapse; width: 100%; font-family: Cairo, sans-serif; border-radius: 10px;'>";
+    while ($row = mysqli_fetch_assoc($result)) {
+        if ($needheaders) {
+            $needheaders = false;
+            echo "<tr style='background-color: #E9E4E4; color: black; font-size: 15px;'>";
+            foreach ($row as $key => $value) {
+                echo "<th style='padding: 10px; text-align: center; color: black; font-size: 15px;'>{$key}</th>";
             }
-            foreach($row as $key => $value){
-                //within quotes need proper html for tuple display $value
-                echo "{$value}   ";
-            }
-            echo "<br>";
+            echo "</tr>";
         }
+        echo "<tr>";
+        foreach ($row as $key => $value) {
+            echo "<td style='background-color: #ffffff; text-align: center; padding: 10px; font-size: 15px;'>{$value}</td>";
+        }
+        echo "</tr>";
     }
+    echo "</table>";
+}
 ?>

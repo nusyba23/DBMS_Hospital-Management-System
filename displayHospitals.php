d304eefe3142f9b76092a4c31fa0069902a5b1f7
Farida152005 <farida.elogueil@ucalgary.ca>
2024-12-02 00:21:39 -0700

    display hospital working yay!

--- displayHospitals.php
+++ displayHospitals.php
@@ -7,20 +7,24 @@
 
     $needheaders = true;
     if(mysqli_num_rows($result) > 0){
+        echo "<table border=1, style='border-collapse: collapse; width: 100%; font-family: Cairo, sans-serif; border-radius: 20px;'>";
         while($row = mysqli_fetch_assoc($result)){
             if($needheaders){
                 $needheaders = false;
+                echo "<tr style='background-color: #E9E4E4, color: black, font-size: 15px;>";
                 foreach($row as $key => $value){
                     //within quotes need proper html for table hearder $key = attribute
-                    echo "{$key}   ";
+                    echo "<th style = 'padding: 10px; text-align: center; color: black, font-size: 15px; '{$key}</th> ";
                 }
-                echo "<br>";
+                echo "<tr>";
             }
             foreach($row as $key => $value){
+                echo "<th style='background-color: #E9E4E4, color: black, font-size: 15px; font-family: Cairo, sans-serif;";
                 //within quotes need proper html for tuple display $value
-                echo "{$value}   ";
+                echo "<th style = 'text-align: center; padding: 10px'{$value}</th>";
             }
-            echo "<br>";
+            echo "<tr>";
         }
+        echo "</table>";
     }
 ?>
